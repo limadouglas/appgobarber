76893a69f2282c930c76c6f82290de5cf4af5c99
Douglas Henrique <lima.dougglas@gmail.com>
2020-10-29 22:22:03 -0300

    update router

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,7 +1,10 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
+import CreateAppointments from '../pages/CreateAppointments';
+import AppointmentsCreated from '../pages/AppointmentsCreated';
 
 import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
 const App = createStackNavigator();
 
@@ -13,6 +16,10 @@ const AppRoutes: React.FC = () => (
     }}
   >
     <App.Screen name="Dashboard" component={Dashboard} />
+    <App.Screen name="CreateAppointments" component={CreateAppointments} />
+    <App.Screen name="AppointmentCreated" component={AppointmentsCreated} />
+
+    <App.Screen name="Profile" component={Profile} />
   </App.Navigator>
 );
 
