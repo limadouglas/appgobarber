6fa1d91fa8224413b9b87083594c7505dd0bbbd7
Douglas Henrique <lima.dougglas@gmail.com>
2020-11-04 13:04:41 -0300

    Create Appointments

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -8,9 +8,16 @@ import React, {
 import AsyncStorage from '@react-native-community/async-storage';
 import api from '../services/api';
 
+interface User{
+  id: string;
+  name: string;
+  email: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: any;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -19,7 +26,7 @@ interface SignInCredentials {
 }
 
 interface AuthContextData {
-  user: any;
+  user: User;
   loading: boolean;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
@@ -39,6 +46,7 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
       setLoading(false);
@@ -56,6 +64,9 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:token', token],
       ['@GoBarber:user', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
     setData({ token, user });
   }, []);
 
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,91 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import React, { useCallback, useEffect, useState } from 'react';
+import api from '../../services/api';
 import { useAuth } from '../../hooks/auth';
+import{Container,
+   Header,
+   HeaderTitle,
+   UserName,
+   ProfileButton,
+   UserAvatar,
+   ProviderList,
+   ProviderListTitle,
+   ProviderContainer,
+   ProviderAvatar,
+   ProviderInfo,
+   ProviderName,
+   ProviderMetaText,
+   ProviderMeta } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+
+export interface Provider{
+  id: string;
+  name: string;
+  url_avatar: string;
+}
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  const { signOut, user } = useAuth();
+  const {navigate} = useNavigation();
+
+  useEffect(() => {
+    api.get('providers').then(response=>{
+      setProviders(response.data);
+    })
+  }, [])
+
+  const navigateToProfile = useCallback(() => {
+      //navigate('Profile');
+      signOut()
+    },
+    [signOut],
+  )
+
+  const navigateToCreateAppointments = useCallback(
+    (providerId: string) => {
+      navigate('CreateAppointments', {providerId})
+    }, [navigate],
+  )
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem Vindo, {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{uri: user.avatar_url}}/>
+        </ProfileButton>
+      </Header>
+
+      <ProviderList
+      ListHeaderComponent={<ProviderListTitle>Cabeleireiros</ProviderListTitle>}
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({item: provider}) => (
+          <ProviderContainer onPress={()=>{navigateToCreateAppointments(provider.id)}}>
+            <ProviderAvatar source={{uri: provider.url_avatar}} />
+            <ProviderInfo>
+              <ProviderName>{provider.name}</ProviderName>
+
+              <ProviderMeta>
+                <Icon name="calendar" size={14} color="#ff9000"/>
+                <ProviderMetaText>Segunda às sexta</ProviderMetaText>
+              </ProviderMeta>
+
+              <ProviderMeta>
+                <Icon name="clock" size={14} color="#ff9000"/>
+                <ProviderMetaText>8h às 18h</ProviderMetaText>
+              </ProviderMeta>
+            </ProviderInfo>
+          </ProviderContainer>
+        )}
+      />
+    </Container>
   );
 };
 
