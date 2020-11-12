1f86c644da8aafc865ca05836889ddc45e0bf10c
Douglas Henrique <lima.dougglas@gmail.com>
2020-11-12 10:08:14 -0300

    Nivel 5: Agendamento -  Alternando entre providers

--- src/pages/CreateAppointments/index.tsx
+++ src/pages/CreateAppointments/index.tsx
@@ -1,11 +1,19 @@
 import { useNavigation, useRoute } from '@react-navigation/native';
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/Feather';
+import api from '../../services/api';
 import { useAuth } from '../../hooks/auth';
-import { Container, Header, HeaderTitle, BackButton, UserAvatar } from './styles';
+import { Container, Header, HeaderTitle, BackButton, UserAvatar, ProviderListContainer, ProviderList, ProviderContainer, ProviderAvatar, ProviderName} from './styles';
+
 
 interface RouteParams{
   providerId: string;
+}
+
+export interface Provider{
+  id: string;
+  name: string;
+  url_avatar: string;
 }
 
 const CreateAppointments: React.FC = ()=> {
@@ -14,9 +22,23 @@ const CreateAppointments: React.FC = ()=> {
   const route = useRoute();
   const { goBack } = useNavigation();
 
-  const { providerId } = route.params as RouteParams;
+  const routeParams = route.params as RouteParams;
+
+  const [providers, setProviders] = useState<Provider[]>([]);
+  const [selectedProvider, setSelectedProvider] = useState(routeParams.providerId)
+
 
   const navigateBack = useCallback(() => {goBack()}, [goBack])
+
+  const handleSelectProvider = useCallback((providerId)=>{
+    setSelectedProvider(providerId)
+  }, [setSelectedProvider])
+
+  useEffect(() => {
+    api.get('providers').then(response=>{
+      setProviders(response.data);
+    })
+  }, [])
 
   return (
     <Container>
@@ -27,6 +49,26 @@ const CreateAppointments: React.FC = ()=> {
         <HeaderTitle>Cabeleireiro</HeaderTitle>
         <UserAvatar source={{ uri: user.avatar_url }} />
       </Header>
+
+      ProviderListContainer,
+      ProviderList,
+      ProviderContainer,
+      ProviderAvatar,
+      ProviderName,
+
+      <ProviderListContainer>
+        <ProviderList
+          data={providers}
+          keyExtractor={(provider)=>provider.id}
+          renderItem={({item: provider}) => (
+            <ProviderContainer selected={selectedProvider === provider.id} onPress={()=>handleSelectProvider(provider.id)}>
+              <ProviderAvatar>{provider.url_avatar}</ProviderAvatar>
+              <ProviderName selected={selectedProvider === provider.id}>{provider.name}</ProviderName>
+            </ProviderContainer>
+          )}
+        />
+
+      </ProviderListContainer>
     </Container>
   )
 }
--- src/pages/CreateAppointments/styles.ts
+++ src/pages/CreateAppointments/styles.ts
@@ -1,5 +1,14 @@
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import styled from 'styled-components/native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { FlatList, RectButton } from 'react-native-gesture-handler';
+import { Provider } from './index';
+
+interface ProviderContainerProps{
+  selected: boolean;
+}
+interface ProviderNameProps{
+  selected: boolean;
+}
 
 export const Container = styled.View`
   flex:1;
@@ -31,3 +40,34 @@ export const UserAvatar = styled.Image`
   border-radius: 28px;
   margin-left: auto;
 `;
+
+export const ProviderListContainer = styled.View`
+  height: 112px;
+`;
+
+export const ProviderList = styled(FlatList as new()=> FlatList<Provider>)`
+  padding: 32px 24px;
+`;
+
+export const ProviderContainer = styled(RectButton)<ProviderContainerProps>`
+  background: ${props=> props.selected ? '#ff9000': '#3e3b47'};
+  flex-direction: row;
+  align-items: center;
+  padding: 8px 12px;
+  margin-right: 16px;
+  border-radius: 10px;
+`;
+
+export const ProviderAvatar = styled.View`
+  width: 32px;
+  height: 32px;
+  border-radius: 16px;
+`;
+
+export const ProviderName = styled.Text<ProviderNameProps>`
+  margin-left: 8px;
+  font-family: 'RobotoSlab-Medium';
+  font-size: 16px;
+  color: ${props=> props.selected ? '#232129': '#f4ede8'};
+`;
+
