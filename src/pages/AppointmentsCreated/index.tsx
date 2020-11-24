dd6e5f8deae9a290b3b8bb3bcd799baeb61a2bf4
Douglas Henrique <lima.dougglas@gmail.com>
2020-11-24 09:45:00 -0300

    Nivel 5: Perfil - Criando tela de perfil

--- src/pages/AppointmentsCreated/index.tsx
+++ src/pages/AppointmentsCreated/index.tsx
@@ -1,8 +1,48 @@
-import React from 'react';
-import {View} from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { useCallback, useMemo } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+import { Container, Title, Description, OkButton, OkButtonText } from './styles'
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
+interface RouteParams{
+  date: number;
+}
 
 const AppointmentsCreated: React.FC = ()=>{
-  return <View />
+  const{ reset } = useNavigation();
+  const {params} = useRoute();
+  const routeParams = params as RouteParams;
+  const handleOkPressed = useCallback(
+    () => {
+      reset({
+        routes: [{name: 'Dashboard'}],
+        index: 0,
+      })
+    },
+    [reset],
+  )
+
+  const formattedDate = useMemo(() => {
+    return format(
+      routeParams.date,
+      "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH:mm'h'",
+      {locale: ptBR},
+
+    );
+  }, [routeParams.date]);
+
+  return (
+    <Container>
+      <Icon name='check' size={80} color='#04d361'/>
+      <Title>Agendamento concluido</Title>
+      <Description>{formattedDate}</Description>
+
+      <OkButton onPress={handleOkPressed}>
+        <OkButtonText>OK</OkButtonText>
+      </OkButton>
+    </Container>
+  )
 }
 
 export default AppointmentsCreated;
--- src/pages/CreateAppointments/index.tsx
+++ src/pages/CreateAppointments/index.tsx
@@ -59,7 +59,7 @@ const CreateAppointments: React.FC = ()=> {
       const date = new Date(selectedDate);
 
       date.setHours(selectedHour);
-      date.setMilliseconds(0);
+      date.setMinutes(0);
 
       await api.post('appointments', {
         provider_id: selectedProvider,
